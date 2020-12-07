4ef9ea76fdf2be50025363128a765800dce87c03
root <devel@libxretia.so>
2020-12-07 13:38:18 +0000

    update

--- res/zjeEdu/player.php
+++ res/zjeEdu/player.php
@@ -1,45 +1,27 @@
-
-<!--
-/*
- * (C) Copyright 2012 Adobe Systems Incorporated. All Rights Reserved.
- *
- * NOTICE:  Adobe permits you to use, modify, and distribute this file in accordance with the 
- * terms of the Adobe license agreement accompanying it.  If you have received this file from a 
- * source other than Adobe, then your use, modification, or distribution of it requires the prior 
- * written permission of Adobe. 
- * THIS CODE AND INFORMATION IS PROVIDED "AS-IS" WITHOUT WARRANTY OF
- * ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
- * PARTICULAR PURPOSE.
- *
- *  THIS CODE IS NOT SUPPORTED BY Adobe Systems Incorporated.
- *
- */
--->
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<!-- saved from url=(0014)about:internet -->
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
 <head>
-<title>视频新闻</title>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>视频新闻</title>
+    <link href="video-js.min.css" rel="stylesheet" type="text/css">
+    <script src="video.js"></script>
+    <script>
+        videojs.options.flash.swf = "video-js.swf";
+    </script>
+    <style>
+    .vjs-default-skin .vjs-big-play-button {
+        left: 250px;
+        top: 200px;
+    }
+    </style>
 </head>
 <body style="background:#333333">
     <center>
-    <img src="top.png"/></br>
-    <object type="application/x-shockwave-flash" 
-    id="SampleMediaPlayback" 
-    name="SampleMediaPlayback" 
-    data="SampleMediaPlayback.swf" 
-    width="640" height="512" align="middle">
-        <param name="quality" value="high">
-        <param name="bgcolor" value="#000000">
-        <param name="allowscriptaccess" value="sameDomain">
-        <param name="allowfullscreen" value="true">
-        <param name="flashvars" value="src=<?php echo $_GET['url'] ?>&amp;streamType=recorded&amp;autoPlay=true&amp;controlBarAutoHide=true&amp;controlBarPosition=bottom">
-    </object>
+        <img src="top.png" />
+        <video width="640" height="512" id="example_video_1" class="video-js vjs-default-skin" controls preload="none" width="640" height="264" data-setup="{}">
+            <source src="<?php echo $_GET['url'] ?>" type="rtmp/flv"/>
+            <p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a></p>
+        </video>
+        <font size="2" color="#222">由 xRetiaWebProxy 强力驱动</font>
     </center>
 </body>
 </html>
-
