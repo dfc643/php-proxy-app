006287a6e21904da019f9d294d70ba5b87b8b5bf
root <devel@libxretia.so>
2020-12-08 14:07:35 +0000

    wlh list

--- vendor/athlon1600/php-proxy/src/Plugin/ProxifyPlugin.php
+++ vendor/athlon1600/php-proxy/src/Plugin/ProxifyPlugin.php
@@ -213,25 +213,31 @@ class ProxifyPlugin extends AbstractPlugin {
 		{
 			if (preg_match('/.{0,10}((\"|\')\/[^+ \"\'\.]*(\.(htm|html|asp|jsp|aspx|php))?[^+ \"\'\.]*(\"|\'))/', $str, $wlh_next_links))
 			{
-				//echo '<pre>'; var_dump($wlh_next_links); die();
-				//__skip_something_noneed__
-				$wlh_check_string = preg_replace('/\s+/', '', $wlh_next_links[0]);
-				if (stripos($wlh_check_string, "src=")
-				|| stripos($wlh_check_string, "href=")
-				|| stripos($wlh_check_string, "action=")
-				|| stripos($wlh_check_string, "browser") 	//__browser_check__
-				|| stripos($wlh_check_string, "validate") 	//__user_validate__
-				) {
-					$nothong = 1;
-				}
-				else
-				{
-					$wlh_next_link = preg_replace("/(\"|\')/", "", $wlh_next_links[1]);
-					if ($wlh_next_link != "/") { //__anti_loop__
-						$wlh_next_link = proxify_url($wlh_next_link, $this->base_url);
-						$response->setStatusCode(302);
-						$response->setContent("");
-						return header("Location: $wlh_next_link");
+				// WLH_LIST
+				foreach(file($_SERVER['DOCUMENT_ROOT']."/config/wlh.list") as $line) {
+					if(strtolower($line) === trim(strtolower($url_host))) {
+						
+						//echo '<pre>'; var_dump($wlh_next_links); die();
+						//__skip_something_noneed__
+						$wlh_check_string = preg_replace('/\s+/', '', $wlh_next_links[0]);
+						if (stripos($wlh_check_string, "src=")
+						|| stripos($wlh_check_string, "href=")
+						|| stripos($wlh_check_string, "action=")
+						|| stripos($wlh_check_string, "browser") 	//__browser_check__
+						|| stripos($wlh_check_string, "validate") 	//__user_validate__
+						) {
+							$nothong = 1;
+						}
+						else
+						{
+							$wlh_next_link = preg_replace("/(\"|\')/", "", $wlh_next_links[1]);
+							if ($wlh_next_link != "/") { //__anti_loop__
+								$wlh_next_link = proxify_url($wlh_next_link, $this->base_url);
+								$response->setStatusCode(302);
+								$response->setContent("");
+								return header("Location: $wlh_next_link");
+							}
+						}
 					}
 				}
 			}
